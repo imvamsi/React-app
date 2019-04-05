be263453aee449e7c5436e8fbfe7399c66e042a9
Vamsi Krishna <vamsi.krishna@cinco.ca>
2019-04-05 14:08:19 -0400

    Table structure modified

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Like from './common/like';
 
-const MoviesTable = (props) => {
-    const {movies, onLike, onDelete, onSort} = props;
+class MoviesTable extends Component {
+
+    raiseSort = path => {
+        const sortColumn = {...this.props.sortColumn};
+
+        if(sortColumn.path===path){
+            sortColumn.order=(sortColumn.order==='asc')?'desc':'asc';
+        }else{
+            sortColumn.path=path;
+            sortColumn.order='asc'; 
+        }
+        this.props.onSort(sortColumn);
+    }
+
+    render() { 
+        const {movies, onLike, onDelete} = this.props;
     return ( 
         <table className="table">
               <thead>
                   <tr>
-                      <th onClick={()=>onSort('title')}>Title</th>
-                      <th onClick={()=>onSort('genre.name')}>Genre</th>
-                      <th onClick={()=>onSort('numberInStock')}>Stock</th>
-                      <th onClick={()=>onSort('dailyRentalRate')}>Rate</th>
+                      <th onClick={()=>this.raiseSort('title')}>Title</th>
+                      <th onClick={()=>this.raiseSort('genre.name')}>Genre</th>
+                      <th onClick={()=>this.raiseSort('numberInStock')}>Stock</th>
+                      <th onClick={()=>this.raiseSort('dailyRentalRate')}>Rate</th>
                       <th></th>
                       <th></th>
                   </tr>
@@ -42,6 +56,13 @@ const MoviesTable = (props) => {
               </tbody>
           </table>
      );
+
+    }
 }
+ 
+
+
+
+    
  
 export default MoviesTable;
