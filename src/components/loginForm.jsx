ba3fe705ae1b87979bb8dc90ea0940b65e287522
Vamsi Krishna <vamsi.krishna@cinco.ca>
2019-04-17 16:00:25 -0400

    form work

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    state = {  }
+    state = { 
+        account:{username:'', password:''}
+     }
+
+    
+    
+    handleChange = e => {
+        const account = {...this.state.account};
+        account[e.currentTarget.name]=e.currentTarget.value;
+        this.setState({account:account});
+    }
+    
+    
+    
+    
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log("Submitted");
+    }
     render() { 
         return ( 
             
            <div className="container">
                <h1> Login </h1>
-               <form>
+               <form onSubmit={this.handleSubmit}>
                    <div className="form-group">
                      <label htmlFor="">Username</label>
-                     <input type="text" className="form-control"/>
+                     <input value ={this.state.username} name="username" onChange={this.handleChange} type="text" className="form-control"/>
                    </div>
 
                    <div className="form-group">
                      <label htmlFor="">Password</label>
-                     <input type="text" className="form-control"/>
+                     <input value={this.state.password} name="password" onChange={this.handleChange} type="text" className="form-control"/>
                    </div>
                    <button className = "btn btn-primary">Login</button>
                </form>    
